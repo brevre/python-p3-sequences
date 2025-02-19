f6c001d015bd0431f38a732467cd228c4237589a
brevre <brendamakena09@gmail.com>
2025-02-19 22:16:00 +0300

    Added Fibonacci sequence function

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,15 +1,11 @@
-
 def print_fibonacci(length):
     if length <= 0:
         print([])
-    elif length == 1:
-        print([0])
-    else:
-        fibonacci_sequence = [0, 1]
-        
-        while len(fibonacci_sequence) < length:
-            next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
-            fibonacci_sequence.append(next_number)
+        return
+    
+    fibonacci = [0, 1] if length > 1 else [0]
 
-        print(fibonacci_sequence)
-print_fibonacci(9)
+    while len(fibonacci) < length:
+        fibonacci.append(fibonacci[-1] + fibonacci[-2])
+
+    print(fibonacci)
